9a04da121036693a7c9fe520b1d6e66e895838f0
michelle-E12 <michelle_espinoza12@outlook.com>
2022-06-07 15:59:18 -0600

    Agregando Entity Framework Code First

--- HNClothingShop/HNClothingShop.BL/ProductosBL.cs
+++ HNClothingShop/HNClothingShop.BL/ProductosBL.cs
@@ -8,39 +8,21 @@ namespace HNClothingShop.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Carteras";
-            producto1.Precio = 1000;
-            producto1.Existencia = 13;
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Camisetas";
-            producto2.Precio = 350;
-            producto2.Existencia = 190;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
 
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Jeans";
-            producto3.Precio = 520;
-            producto3.Existencia = 150;
-
-            var producto4 = new Producto();
-            producto4.Id = 4;
-            producto4.Descripcion = "Vestidos";
-            producto4.Precio = 530;
-            producto4.Existencia = 12;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
-
-            return listadeProductos;
         }
     }
 }
