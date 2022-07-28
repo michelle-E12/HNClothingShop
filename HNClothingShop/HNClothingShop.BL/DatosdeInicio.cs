def1da14c521d55439b74d229d40bb8645c71ca9
michelle-E12 <michelle_espinoza12@outlook.com>
2022-07-27 19:31:27 -0600

    Agregando la funcionalidad de login

--- HNClothingShop/HNClothingShop.BL/DatosdeInicio.cs
+++ HNClothingShop/HNClothingShop.BL/DatosdeInicio.cs
@@ -12,10 +12,20 @@ namespace HNClothingShop.BL
         protected override void Seed(Contexto contexto)
         {
             var nuevoUsuario = new Usuario();
-            nuevoUsuario.Nombre = "admin";
+            nuevoUsuario.Nombre = "michelle";
             nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
 
+            var nuevoUsuario2 = new Usuario();
+            nuevoUsuario2.Nombre = "edgar";
+            nuevoUsuario2.Contrasena = Encriptar.CodificarContrasena("456");
+
+            var nuevoUsuario3 = new Usuario();
+            nuevoUsuario3.Nombre = "allan";
+            nuevoUsuario3.Contrasena = Encriptar.CodificarContrasena("789");
+
             contexto.Usuarios.Add(nuevoUsuario);
+            contexto.Usuarios.Add(nuevoUsuario2);
+            contexto.Usuarios.Add(nuevoUsuario3);
 
             base.Seed(contexto);
         }
